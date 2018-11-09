4a4bc8330ac7cff798b2ae9e936b06109874a528
tim <tim@gmail.com>
2018-11-09 20:11:45 +0800

    add

---
+++ Clothes/src/Clothes.java
@@ -0,0 +1,44 @@
+package Clothes;
+
+public class Clothes{
+
+	private String size;
+	private String color;
+
+	public Clothes(){}
+	
+	public Clothes(String size){
+		this();
+		setSize(size);
+	}
+	
+	public Clothes(String size,String Color){
+		this(size);
+		setColor(color);
+	}
+	
+	
+	public String getSize(){
+		return this.size;
+	}
+	
+	public void setSize(String size){
+		this.size = size;
+	}
+	
+	public String getColor(){
+		return this.color;
+	}
+	
+	public void setColor(String color){
+		this.color = color;
+	}
+
+	public int getPrize(){
+		return this.prize;
+	}
+	
+	public void setPrize(prize){
+		this.prize = prize;
+	}
+}
